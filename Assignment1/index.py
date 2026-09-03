9ef8cd46ab524ecc4a6443d691fdb15bc233bdc4
Ryan Vu <vuryan612@gmail.com>
2026-09-03 13:21:32 -0700

    A1: first implementation

--- Assignment1/index.py
+++ Assignment1/index.py
@@ -8,6 +8,7 @@
 #-------------------------------------------------------------------------
 
 # Importing Python libraries
+from collections import defaultdict
 import pandas as pd
 
 # Reading the document collection
@@ -15,10 +16,14 @@ data = pd.read_csv("collection.csv")
 
 # Defining the dictionary used for lemmatization
 # --> add your Python code here
-lemmas = ?
+lemmas = {}
+lemmas["homes"] = "home"
+lemmas["increases"] = "rise"
+lemmas["rising"] = "rise"
+lemmas["sales"] = "sale"
 
 # Creating the data structure that will store the inverted index
-invertedIndex = {}
+invertedIndex = defaultdict(list)
 
 # Processing each document in the collection
 for i, row in data.iterrows():
@@ -27,19 +32,32 @@ for i, row in data.iterrows():
     text = row["Text"]
 
     # Applying surface-level normalization
-    # --> add your Python code here
+    text = text.lower()
+    text = text.translate(str.maketrans("", "", ".!?"))
 
     # Tokenizing the document
-    # --> add your Python code here
+    words = text.split(' ')
 
     # Applying lemmatization
     # --> add your Python code here
+    for i in range(len(words)):
+        if words[i] in lemmas:
+            words[i] = lemmas[words[i]]
 
     # Building the inverted index
     # --> add your Python code here
+    for word in words:
+        invertedIndex[word].append(docID)
 
 # Printing the inverted index with terms ordered alphabetically
 # Expected format:
 # term1 : ['Doc1', 'Doc2']
 # term2 : ['Doc3']
 # --> add your Python code here
+
+# Alphabetize
+invertedIndex = dict(sorted(invertedIndex.items()))
+
+# Print inverted index
+for key, value in invertedIndex.items():
+    print(f"{key} : {value}")
